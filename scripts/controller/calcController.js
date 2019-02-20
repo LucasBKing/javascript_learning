800de5b885880fa992333f4b527a4c03bddd2a57
lucasbking <ldsbarbosa@inf.ufpel.edu.br>
2019-02-19 22:24:11 -0300

    Fix some bugs like max number on screen

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -107,7 +107,15 @@ class CalcController {
     }
     
     getResult() {
-        return eval(this._operation.join(""));
+       try {
+            return eval(this._operation.join(""));
+        } catch(e) {
+            setTimeout(() => {
+                this.setError();
+            }, 1)
+            
+        }
+        
     }
 
     getLastItem(isOperator = true) {
