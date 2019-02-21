f6e67a3a7991148d1f2dcd3da4062324b24495de
lucasbking <ldsbarbosa@inf.ufpel.edu.br>
2019-02-21 17:57:27 -0300

    using local storage

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -199,9 +199,9 @@ class UserController {
     getUsersStorage() {
         let users = [];
 
-        if(sessionStorage.getItem("users")) {
+        if(localStorage.getItem("users")) {
 
-            users = JSON.parse(sessionStorage.getItem("users"));
+            users = JSON.parse(localStorage.getItem("users"));
         }
 
         return users;
@@ -225,7 +225,8 @@ class UserController {
         
         users.push(data);
         // setItem(key, value)
-        sessionStorage.setItem("users", JSON.stringify(users));
+        //sessionStorage.setItem("users", JSON.stringify(users));
+        localStorage.setItem("users", JSON.stringify(users));
     };
 
     addLine(dataUser) {
