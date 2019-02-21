93b228b537361f9e8cc0efe2a71ab21274654a28
lucasbking <ldsbarbosa@inf.ufpel.edu.br>
2019-02-21 17:27:20 -0300

    deleting users

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -206,7 +206,7 @@ class UserController {
             <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
                 <button type="button" class="btn btn-edit btn-primary btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                <button type="button" class="btn btn-delete btn-danger btn-xs btn-flat">Excluir</button>
             </td>
         `;
 
@@ -239,6 +239,14 @@ class UserController {
     };
 
     addEventsTr(tr) {
+        tr.querySelector(".btn-delete").addEventListener("click", e => {
+            if (confirm("Deseja realmente excluir?")) {
+                tr.remove();
+
+                this.updateCount();
+            }
+        });
+
         tr.querySelector(".btn-edit").addEventListener("click", e => {
             let jsonUsers = JSON.parse(tr.dataset.user);
 
