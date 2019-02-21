7e64cabd5e7bb18f11e9cb36602cdaa20188ef10
lucasbking <ldsbarbosa@inf.ufpel.edu.br>
2019-02-21 16:06:30 -0300

    creating the events to update and cancel the update

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,6 +4,7 @@ class UserController {
         this.tableEl = document.getElementById(tableId);
 
         this.onSubmit();
+        this.onEdit();
     }
 
     // Adding an event when user press the submit button
@@ -34,6 +35,24 @@ class UserController {
                     console.error(e)
                 });
         });
+    };
+
+    onEdit() {
+
+        document.querySelector("#box-user-update .btn-cancel").addEventListener("click", e => {
+            this.showPanelCreate();
+        });
+        
+    };
+
+    showPanelCreate() {
+        document.querySelector("#box-user-create").style.display = "block";
+        document.querySelector("#box-user-update").style.display = "none";
+    };
+
+    showPanelUpdate() {
+        document.querySelector("#box-user-create").style.display = "none";
+        document.querySelector("#box-user-update").style.display = "block";
     };
 
     getPhoto() {
@@ -130,10 +149,17 @@ class UserController {
             <td>${(dataUser.admin) ? 'Yes' : 'No'}</td>
             <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
-                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-edit btn-primary btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
         `;
+
+        tr.querySelector(".btn-edit").addEventListener("click", e => {
+            console.log(JSON.parse(tr.dataset.user));
+
+            this.showPanelUpdate();
+
+        });
 
         this.tableEl.appendChild(tr);
 
